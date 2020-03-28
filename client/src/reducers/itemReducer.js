99d3b68e0fa61bd1c048ebeec774cc2cf4f25b3d
Luke Prosser <luke.prosser@cloud-iq.com>
2020-03-28 17:17:47 +0000

    Introduce state management with Redux

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+
+const initialState = {
+  items: [
+    { id: uuidv4(), name: 'Eggs' },
+    { id: uuidv4(), name: 'Milk' },
+    { id: uuidv4(), name: 'Steak' },
+    { id: uuidv4(), name: 'Bread' }
+  ]
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case GET_ITEMS:
+      return {
+        ...state
+      }
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload) // payload defined in itemActions
+      }
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      }
+    default:
+      return state;
+  }
+}
